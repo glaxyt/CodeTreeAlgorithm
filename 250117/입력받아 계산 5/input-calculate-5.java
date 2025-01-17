5bcd1210426fbc2021eed0c08964bcc26d352806
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2025-01-17 22:57:31 +0900

    Title: 입력받아 계산 5, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 250117/입력받아 계산 5/input-calculate-5.java
+++ 250117/입력받아 계산 5/input-calculate-5.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = nextInt();
-        int b = nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
         System.out.println(a + b);
     }
