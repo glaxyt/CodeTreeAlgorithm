28b56b69892290f617fe99ab3d1b3165ae34eb00
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2025-02-08 23:07:51 +0900

    Title: 정수 명령 처리, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250208/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,36 @@
+import java.util.Stack
+import java.util.Scanner
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Stack<Integer> s = new Stack<>();
+    
+        for(int i = 0; i < n; i++) {
+            String command = st.next();
+
+            if(command.equals("push")) {
+                int num = nextInt();
+                s.push(num);
+            } else if(command.equals("pop")) {
+                System.out.println(s.pop());
+            } else if(command.equals("size")) {
+                System.out.println(s.size());
+            } else if(command.equals("empty")) {
+                if(s.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            } else {
+                Sytsem.out.println(s.peek());
+            }
+            
+
+        }
+
+    }
+}
