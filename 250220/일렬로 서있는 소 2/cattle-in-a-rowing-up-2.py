6283dc90a7f4113bb8653945b4832352c40fc73e
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2025-02-20 16:59:29 +0900

    Title: 일렬로 서있는 소 2, Time: 64ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250220/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 250220/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -3,9 +3,9 @@ A = list(map(int, input().split()))
 
 # Write your code here!
 ans = 0
-for i in range(N):
-    for j in range(i, N):
-        for k in range(j, N):
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
             if A[i] < A[j] and A[j] < A[k]:
                 ans += 1
 
