31dd71aee1ae60652cd6888aa9e62db026fe93ae
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2025-03-05 16:31:45 +0900

    Title: 사각형 채우기 3, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250305/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+# Please write your code here.
+memo = [0] * (n+3)
+accumulate_total = [0] * (n+3)
+
+memo[0] = 1
+memo[1] = 2
+memo[2] = 7
+memo[3] = 22
+
+def dp(n):
+    if n <= 3:
+        return memo[n]
+    else:
+        for i in range(4, n+1):
+            memo[i] = (2 * memo[i-1]) + (3 * memo[i-2]) + (2 * memo[i-3])
+    return memo[n]
+
+print(dp(n))
