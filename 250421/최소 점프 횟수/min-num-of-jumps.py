42385ae65295d139d459d3939a6157461e76e461
SungHyun Do <52828205+glaxyt@users.noreply.github.com>
2025-04-21 21:30:15 +0900

    Title: 최소 점프 횟수, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 250421/최소 점프 횟수/min-num-of-jumps.py
+++ 250421/최소 점프 횟수/min-num-of-jumps.py
@@ -2,24 +2,25 @@ from collections import deque
 
 n = int(input())
 num = list(map(int, input().split()))
-visited = [False] * n
 # Please write your code here.
-queue = deque([0])
-visited[0] = 1
+def solution():
+    visited = [False] * n
+    queue = deque([0])
+    visited[0] = 1
 
-while queue:
-    cx = queue.pop()
-    
-    if cx == n-1:
-        print(visited[cx] - 1)
-        break
+    while queue:
+        cx = queue.pop()
+        
+        if cx == n-1:
+            return visited[cx] - 1
 
-    jump = num[cx]
+        jump = num[cx]
 
-    for i in range(1, jump + 1):
-        nx = cx + i
-        if 0 <= nx < n and not visited[nx]:
-            visited[nx] = visited[cx] + 1
-            queue.appendleft(nx)
-else:
-    print(-1)
+        for i in range(1, jump + 1):
+            nx = cx + i
+            if 0 <= nx < n and not visited[nx]:
+                visited[nx] = visited[cx] + 1
+                queue.appendleft(nx)
+    return -1
+
+print(solution())
